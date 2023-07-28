d18f543aa24ef74d096be74526d05473404d5dd7
jburky15 <jburky15@gmail.com>
2023-07-27 20:18:48 -0400

    Added construction sign to homepage

--- src/data/dogs.js
+++ src/data/dogs.js
@@ -4,14 +4,14 @@ export const dogs = [
         breed: "Labrador Retriever",
         age: 14,
         description: "Charlie is an old guy, but he still has a lot of love in his heart and absolutely loves attention!",
-        image: "/images/charlie.jpeg",
+        image: "/images/charlie.webp",
     },
     {
         name: "Scooter",
         breed: "Jack Russell",
         age: 1,
         description: "A hyper-active but fun loving doggo, great with kids!",
-        image: "/images/scooter.jpeg"
+        image: "/images/scooter.webp"
     },
     {
         name: "Daisy",
@@ -25,28 +25,28 @@ export const dogs = [
         breed: "Pitbull",
         age: 1,
         description: "Toby is a good boy who needs a good, loving home that is always active and ready to play at any moment!",
-        image: "/images/toby.jpeg"
+        image: "/images/toby.webp"
     },
     {
         name: "Akuma",
         breed: "Akita",
         age: 6,
         description: "Akuma will make a great addition to any family. Being an Akita, he is very loyal and very protective, especially of the youngest members of his pack.",
-        image: "/images/akuma.jpg",
+        image: "/images/akuma.webp",
     },
     {
         name: "Rusty",
         breed: "Cavalier King Charles",
         age: 3,
         description: "His bark is worse than his bite! This little guy wants nothing than to be you new favorite lap warmer, he doesn't like long walks, but prefers long naps with his family.",
-        image: "/images/rusty.jpg"
+        image: "/images/rusty.webp"
     },
     {
         name: "Cooper",
         breed: "German Shepherd",
         age: 9,
         description: "Cooper is a recently retired K-9 unit dog who is looking for a nice, calm home to live out the rest of his days in peace and relaxation.",
-        image: "/images/cooper.jpg",
+        image: "/images/cooper.webp",
     },
     {
         name: "Nugget",
@@ -60,6 +60,6 @@ export const dogs = [
         breed: "Irish Wolfhound",
         age: 10,
         description: "McGregor is a very special fella. He is hard of hearing, but loves to get close to people and gives lots of extra kisses and paw shakes.",
-        image: "/images/mcgregor.jpg",
+        image: "/images/mcgregor.webp",
     }
 ]
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css'
+import underConstruction from '../../images/under-construction.webp'
 
 const Home = () => {
   return (
     <>
       <div>
+      <img src={ underConstruction } alt="" className='underConstruction' />
         <div className='mainContent'>
           <h1>This is the Home Page For Doggo Adoption Network!</h1>
           <p>Please pardon our dust, we are currently under construction!</p>
