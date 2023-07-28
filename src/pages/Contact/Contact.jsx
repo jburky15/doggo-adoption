c926cce5721e57e56cbca102f4f93a698faa0db7
jburky15 <jburky15@gmail.com>
2023-07-28 17:21:05 -0400

    Added fontawesome, fixed contact page a bit

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ContactForm from '../../components/Contact/ContactForm';
 import './Contact.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const ContactPage = () => {
   return (
@@ -21,15 +23,14 @@ const ContactPage = () => {
             <ContactForm />
         </div>
         <div className="mapWrapper">
-                <h1>Google Maps Placeholder</h1>
-                <h3 className='phoneNumberHeader'>Our Phone#:</h3>
-                <p className='phoneNumber'>888-888-8888</p>
+                <p className='phoneNumber'><FontAwesomeIcon icon={faPhone} /> 888-888-8888</p>
+                <p className='address'><FontAwesomeIcon icon={faLocationDot} /> 11 Pottery Rd, Chittenango, NY</p>
                 <iframe 
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d649.2717155260995!2d-75.8693647580722!3d43.05970475894174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d985316f8db50f%3A0x5392acd4bc2ea38c!2s11%20Pottery%20Rd%2C%20Chittenango%2C%20NY%2013037!5e0!3m2!1sen!2sus!4v1690543924282!5m2!1sen!2sus"
                   title="Doggo Adoption Network"
+                  className='gMap'
                   width="600" 
-                  height="450" 
-                  style={{ border: "0" }} 
+                  height="450"
                   allowfullscreen="" 
                   loading="lazy" 
                   referrerpolicy="no-referrer-when-downgrade">
