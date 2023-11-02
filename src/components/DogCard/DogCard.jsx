68f64969fc10b62a4f0d6ce6e75679eacdc5f386
jburky15 <jburky15@gmail.com>
2023-11-01 22:42:53 -0400

    Smol Changes

--- src/components/DogCard/DogCard.jsx
+++ src/components/DogCard/DogCard.jsx
@@ -3,12 +3,12 @@ import "./dogcard.css"
 import { AdoptButton } from '../adoptButton/AdoptButton';
 
 export const DogCard = ({ name, breed, age, description, image }) => {
-    const year = age < 2 ? 'year' : 'years';
+    const year = age < 2 ? 'Year' : 'Years';
 
     return (
         <div className="dog-card">
             <img src={image} alt={name} />
-            <h3>{name} • {age} {year} old</h3>
+            <h3>{name} • {age} {year} Old</h3>
             <p className='dogBreed'>{breed}</p>
             <p className='dogDescription'>{description}</p>
             <AdoptButton />
