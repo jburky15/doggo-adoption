3c4ad6df66f65406e15bb44701b0f3e0ec4b7fc1
jburky15 <jburky15@gmail.com>
2024-08-07 23:29:12 -0400

    Removed useless checkbox, just need to finish submit button and homepage

--- src/pages/AdoptForm/AdoptForm.jsx
+++ src/pages/AdoptForm/AdoptForm.jsx
@@ -102,8 +102,7 @@ const AdoptForm = () => {
           <input className='radioSelector' type="radio" value="apartment" name="apartment" checked={isHousingRadioChecked("apartment")} onChange={handleHousingRadioSelect}/> Apartment
         </div>
         <div className="rentalCheck">
-            <FormCheckboxes label="(Is this a rental property?)   Yes"/>
-            <FormCheckboxes label="No"/>
+            <FormCheckboxes label="Is this a rental property? " />
           </div>
         <div className='landlordInfo'>
           <h3>If you rent:</h3>
@@ -114,6 +113,7 @@ const AdoptForm = () => {
         <input className='radioSelector' type="radio" value="yes" name="yes" checked={isPetRadioChecked("yes")} onChange={handlePetRadioSelect}/> Yes
         <input className='radioSelector' type="radio" value="no" name="no" checked={isPetRadioChecked("no")} onChange={handlePetRadioSelect}/> No
         <h4>If yes, please specify:</h4>
+        <h5>Separate each with a comma ( , )</h5>
           <FormInputs name="otherInput" label="OtherInput" placeholder="Rabbit" />
         
         <button className='adoptionSubmit'>Submit</button>
